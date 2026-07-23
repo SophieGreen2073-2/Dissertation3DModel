c8af5fe2ebaed6c8b6180d89c568f413bef79b29
Sophie Green <sophie.green-5@postgrad.manchester.ac.uk>
2026-07-23 13:43:35 +0100

    Initial quadcopter control

---
+++ scripts/main.py
@@ -0,0 +1,32 @@
+import time
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+import numpy as np
+
+def main():
+    client = RemoteAPIClient()
+    sim = client.getObject('sim')
+    try:
+        # Start simulation
+        sim.startSimulation()
+
+        # Get the quadcopter target
+        target = sim.getObject("/target")
+
+        waypoints = [
+            [62.925, 16.7, 1.0],   # Takeoff to 1m height
+        ]
+
+        for point in waypoints:
+            set_target(sim, target, point[0], point[1], point[2])
+            time.sleep(4)  # Wait 4 seconds for drone to fly to the waypoint
+
+        # Stop simulation
+        sim.stopSimulation()
+    except(Exception):
+        sim.stopSimulation
+
+def set_target(sim, target, x, y, z):
+    sim.setObjectPosition(target, -1, [x, y, z])
+    print(f"Moving quadcopter to target: ({x}, {y}, {z})")
+
+main()
